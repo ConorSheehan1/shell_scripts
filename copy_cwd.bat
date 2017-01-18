55de2cb6b1ef6c40e1c367901ff8bb05ac01c26e
Conor Sheehan <conor.sheehan.2@ucdconnect.ie>
2017-01-18 00:06:27 +0000

    work around error handling in batch file working to witch between python2 and 3

--- copy_cwd.bat
+++ copy_cwd.bat
@@ -1,2 +1,11 @@
+@rem don't print all commands
 @echo off
+
+@rem try python 3
 python C:\Users\conor\Documents\GitHub\shell_scripts\copy_shell3.py %*
+
+@rem if python3 file fails, call python2 file
+IF %ERRORLEVEL% == 0 Exit /b
+
+python C:\Users\conor\Documents\GitHub\shell_scripts\copy_shell2.py %*
+
